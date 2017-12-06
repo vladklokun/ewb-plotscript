fe240d2c0cbf3db944b78eef85cff14c706189cc
vlad <vklokun@protonmail.ch>
2017-12-06 13:37:27 +0200

    Rewrite setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,18 @@
-#!/usr/bin/unv/ python
+#!/usr/bin/env/python
 # -*- coding: utf-8 -*-
 
-import io
-import os
-import sys
-
-from setuptools import find_packages, setup
-
-# Package metadata
-NAME = 'tinyplot'
-DESCRIPTION = 'A tiny tool for building two-dimensional plots'
-URL = 'https://gitlab.com/vladklokun/plotscript'
-AUTHOR = 'The tinyplot developers'
-VERSION = '0.1.0'
-
-# Package requirements
-REQUIRED = [
-	'matplotlib',
-	'numpy',
-]
+from setuptools import setup
 
 setup(
-	name = NAME,
-	version = VERSION
-	description = DESCRIPTION,
-	author = AUTHOR,
-	packages = find_packages(exclude = ('tests',))
-	url = URL,
-	install_requires = REQUIRED,
+	name = 'tinyplot',
+	version = '0.1.0dev',
+	description = 'A tiny tool for building two-dimensional plots',
+	author = 'The tinyplot developers',
+	packages = ['tinyplot'],
+	url = 'https://gitlab.com/vladklokun/plotscript',
+	install_requires = [
+		'setuptools>=38.0.2',
+		'matplotlib>=2.1.0',
+		'numpy>=1.13.3'
+	],
 )
